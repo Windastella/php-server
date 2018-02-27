cabcd8043fa5b71a6e17e5ee5b75075619ade7a2
Nik Mirza <nik96mirza@gmail.com>
2018-02-28 01:30:58 +0800

    Auto Open Browser on launch

--- main.js
+++ main.js
@@ -6,14 +6,15 @@ option
   .version('0.1.0')
   .usage('[options] <file ...>')
   .option('-p, --port <n>', 'Set Port Number',(val)=>{return val}, 8080)
-  .option('-h, --host <s>', 'Set Host Address',(val)=>{return val}, '0.0.0.0')
+  .option('-h, --host <s>', 'Set Host Address',(val)=>{return val}, 'localhost')
   .option('-d, --dir <s>', 'Set Server Directory',(val)=>{return val}, '')
   .option('-t, --test', 'Testing build')
   .parse(process.argv);
 
 try{
-  
+
   const express = require('express'),
+  opn = require('opn'),
   php = require("node-php"),
   path = require("path");
 
@@ -24,6 +25,8 @@ try{
 
   app.listen(option.port, option.host);
   console.log("Server listening http://"+ option.host + ":" +option.port);
+  opn('http://'+ option.host + ':'+option.port);
+
 }catch(e){
   console.error(e);
 }
